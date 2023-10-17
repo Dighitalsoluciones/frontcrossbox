a79e10d901dbd76b13ef7224d5e69d5c8ccab7eb
Dighitalsoluciones <hernanglomba@gmail.com>
2023-10-17 18:01:01 -0300

    solucion a la carga correcta del detalle de los turnos, para su posterior eliminacion

--- src/app/componentes/menuadmin/adm-reservas/detail-adm-reservas.component.ts
+++ src/app/componentes/menuadmin/adm-reservas/detail-adm-reservas.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
 import { Actividades } from 'src/app/model/actividades';
 import { NuevoUsuario } from 'src/app/model/nuevo-usuario';
 import { Turno } from 'src/app/model/turno';
@@ -26,7 +27,8 @@ export class DetailAdmReservasComponent implements OnInit {
   usuarioLog: any;
   usuarioLogeado: any;
 
-  constructor(private reservaServ: TurnoService, private activatedRoute: ActivatedRoute, private authServ: AuthService, private router: Router, private actividadServ: ActividadesService) { }
+  constructor(private reservaServ: TurnoService, private activatedRoute: ActivatedRoute, private authServ: AuthService,
+     private router: Router, private actividadServ: ActividadesService) { }
 
   ngOnInit(): void {
     const id = this.activatedRoute.snapshot.params['id'];
@@ -36,6 +38,7 @@ export class DetailAdmReservasComponent implements OnInit {
     const authCodificado = sessionStorage.getItem(AUTHORITIES_KEY);
     this.rol = authCodificado ? JSON.stringify(atob(authCodificado)) : []; 
     this.traerUsuario(this.usuarioLogeado);  
+   
   }
 
   traerUsuario(nombreUsuario: string): void{
@@ -51,62 +54,80 @@ export class DetailAdmReservasComponent implements OnInit {
       return false;
     }
 
+    async eliminar(id?: number): Promise<void> {
+      if (id != null) {
+        try {
+          await firstValueFrom(this.reservaServ.delete(id));
+          alert("✔ Registro eliminado correctamente");
+          this.router.navigate(['admin']);
+        } catch (err) {
+          alert("No se pudo borrar el registro");
+        }
+      } else {
+        alert("No se pudo borrar el registro");
+      }
+    }
 
-  eliminar(id?: number){
-    if(id != undefined){
-      this.reservaServ.delete(id).subscribe(data =>{alert("✔ Registro eliminado correctamente");
+    traerDatosUsuario() {
+      if (this.detalleReserva && this.detalleReserva.nombreUsuario != null) {
+        this.authServ.detailName(this.detalleReserva.nombreUsuario).subscribe(data => {
+          this.usuario = data;
+          // Realizar otras operaciones con this.usuario si es necesario
+        });
+      }
+    }
+
+  async guardar(): Promise<void> {
+    try {
+      if (this.usuario.id != null) {
+        await firstValueFrom(this.authServ.update(Number(this.usuario.id), this.usuario));
+        // Operación realizada con éxito
+      }
+    } catch (err) {
+      alert("⛔ No se pudo realizar la acción ⛔");
       this.router.navigate(['admin']);
-      },err =>{alert("No se pudo borrar el registro")},
-      
-       )
-       }else{
-        alert("No se pudo borrar el registro")
-       }
-  }
-
-  traerDatosUsuario(){
-    if(this.detalleReserva.nombreUsuario != null){
-    this.authServ.detailName(this.detalleReserva.nombreUsuario).subscribe(data => {this.usuario = data});
-    this.usuario.clasesTomadas --;
-    this.usuario.suscripcionActual ++;
-    this.guardar();
-    
     }
   }
-
-  guardar(): void{
-    this.authServ.update(Number(this.usuario.id), this.usuario).subscribe(
-     data => {}, err =>{
-       alert("⛔ No se pudo realizar la acción ⛔");
-       this.router.navigate(['admin']);
-     }
-   )
-}
 
 mostrarBotonConf(){
   this.botonConf = true;
   this.noMostrarBtnBorrar = false;
 }
 
-guardarAct(){
-  if(this.detalleReserva.id_actividad != null){
-  this.actividadServ.update(this.detalleReserva.id_actividad, this.actividad).subscribe(data => {}, err =>{alert("⛔ No se pudo realizar la acción ⛔");
-  this.router.navigate(['admin']);
-   }
-  )
- }
+async guardarAct(): Promise<void> {
+  try {
+    if (this.detalleReserva.id_actividad != null) {
+      await firstValueFrom(this.actividadServ.update(this.detalleReserva.id_actividad, this.actividad));
+      // Operación realizada con éxito
+    }
+  } catch (err) {
+    alert("⛔ No se pudo realizar la acción ⛔");
+    this.router.navigate(['admin']);
+  }
 }
 
-devolverCupo(){
-  if(this.detalleReserva.id_actividad != null){
-  this.actividadServ.details(this.detalleReserva.id_actividad).subscribe(data => {this.actividad = data});
-  this.actividad.cupo ++;
-  this.guardarAct();
+devolverCupo() {
+  if (this.detalleReserva.id_actividad != null) {
+    this.actividadServ.details(this.detalleReserva.id_actividad).subscribe(data => {
+      this.actividad = data;
+
+      // Verificar si this.actividad no es null o undefined antes de acceder a cupo
+      if (this.actividad && this.actividad.cupo != null) {
+        this.actividad.cupo++;
+        this.guardarAct();
+      } else {
+        // Manejar el caso en el que this.actividad o this.actividad.cupo es null
+        console.error('this.actividad o this.actividad.cupo es null o undefined');
+      }
+    });
   }
 }
 
 unificarFunciones(){
   this.devolverCupo();
+  this.usuario.clasesTomadas --;
+  this.usuario.suscripcionActual ++;
+  this.guardar();
   this.eliminar(this.detalleReserva.id);
 }
 

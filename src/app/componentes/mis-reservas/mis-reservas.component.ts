8d6aec2b61cd6f92aa82a7c9f6f6e04e3132afe8
Dighitalsoluciones <hernanglomba@gmail.com>
2024-05-22 20:19:40 -0300

    Cambio en forma de visualizar mis reservas, se visualiza sin boton de forma automatica

--- src/app/componentes/mis-reservas/mis-reservas.component.ts
+++ src/app/componentes/mis-reservas/mis-reservas.component.ts
@@ -2,6 +2,7 @@ import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Turno } from 'src/app/model/turno';
+import { SpinnerService } from 'src/app/service/spinner.service';
 import { TurnoService } from 'src/app/service/turno.service';
 
 const USERNAME_KEY = 'AuthUsername';
@@ -12,12 +13,12 @@ const USERNAME_KEY = 'AuthUsername';
   styleUrls: ['./mis-reservas.component.css']
 })
 export class MisReservasComponent implements OnInit {
-  reservas: Turno [] = [];
+  reservas: Turno[] = [];
   usuarioLogeado: any;
   misReservas: any;
   reservasDeHoy: any;
-  
-  constructor(private turnoService: TurnoService, private router: Router) { }
+
+  constructor(private turnoService: TurnoService, private router: Router, private spinnerService: SpinnerService) { }
 
   ngOnInit(): void {
     this.TraerReservas();
@@ -25,29 +26,38 @@ export class MisReservasComponent implements OnInit {
     this.usuarioLogeado = usuarioCodificado ? JSON.parse(atob(usuarioCodificado)) : null;
   }
 
-  TraerReservas(){
-    this.turnoService.lista().subscribe(data => {this.reservas = data});
+  TraerReservas() {
+    this.spinnerService.llamarSpinner();
+    this.turnoService.lista().subscribe(data => {
+      this.reservas = data;
+      this.reservasDelUsuario();
+      this.spinnerService.pararSpinner();
+    }, err => {
+      alert("No se pudieron traer las reservas del usuario, intente nuevamente");
+      this.spinnerService.pararSpinner();
+    });
   }
 
-  reservasDelUsuario(){
+  reservasDelUsuario() {
     this.misReservas = this.reservas.filter(reserva => reserva.nombreUsuario == this.usuarioLogeado);
     this.reservasDeHoy = this.misReservas.filter((reserva: { dia: string; }) => reserva.dia >= formatDate(Date.now(), 'yyyy-MM-dd', 'es'));
   }
 
-  volver(){
+  volver() {
     this.router.navigate(['perfil']);
   }
 
-  eliminar(id?: number){
-    if(id != undefined){
-    this.turnoService.delete(id).subscribe(data =>{alert("Registro eliminado correctamente");
-    this.reservasDelUsuario();
-    },err =>{alert("No se pudo borrar el registro")},
-     )
-     }else{
+  eliminar(id?: number) {
+    if (id != undefined) {
+      this.turnoService.delete(id).subscribe(data => {
+        alert("Registro eliminado correctamente");
+        this.reservasDelUsuario();
+      }, err => { alert("No se pudo borrar el registro") },
+      )
+    } else {
       alert("No se pudo borrar el registro")
-     }
     }
- 
-    
+  }
+
+
 }

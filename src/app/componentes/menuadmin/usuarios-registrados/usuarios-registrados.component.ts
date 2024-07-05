0c8f996068b26925be29a67c5277cbb6ba36b04b
Dighitalsoluciones <hernanglomba@gmail.com>
2024-07-05 14:01:07 -0300

    Solucion a filtado de usuarios en menu adm

--- src/app/componentes/menuadmin/usuarios-registrados/usuarios-registrados.component.ts
+++ src/app/componentes/menuadmin/usuarios-registrados/usuarios-registrados.component.ts
@@ -12,44 +12,58 @@ export class UsuariosRegistradosComponent implements OnInit {
 
   res: any[] = [];
   usuariosRegistrados: any;
-  filtrarUsuariosReg = [];
-  modalEliminarUsuario= "none";
-  fechas: Fecha [] = [];
+  usuariosFiltrados: NuevoUsuario[] = [];
+  filtrarUsuariosReg: string = "";
+  filtro: string = "";
+  modalEliminarUsuario = "none";
+  fechas: Fecha[] = [];
   fechaInicio: any;
   fechaFin: any;
   loading = false;
   usuario: any;
   p: number = 0;
   backendPage = 0;
+  mostrarSoloFiltrados: boolean = false;
+  botonBuscar: boolean = true;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,) { }
 
   ngOnInit(): void {
     this.listaUsuarios();
     setTimeout(() => {
       this.loading = true;
     }, 1200);
-   
+
   }
 
   listaUsuarios(): void {
+    this.mostrarSoloFiltrados = false;
+    this.filtrarUsuariosReg = "";
+    this.p = 0;
+    this.botonBuscar = true;
     this.authService.lista(this.p, 12).subscribe(data => {
       this.usuariosRegistrados = data;
     });
   }
 
-  listaUsuariosConFiltro(page: number): void {
-    this.authService.lista(page, 1000).subscribe(data => {
-      // Filtra los usuarios aquí antes de asignarlos a la variable
-      const usuariosFiltrados = this.usuariosRegistrados.content.filter((usuario: { apellido: never[][]; }) => {
-        // Aplica tu lógica de filtrado (por ejemplo, por nombre o algún otro criterio)
-        // Devuelve true si el usuario debe incluirse en la lista filtrada
-        return usuario.apellido.includes(this.filtrarUsuariosReg);
+  filtrarUsuarios() {
+    this.mostrarSoloFiltrados = true;
+    this.botonBuscar = false;
+    // Valor del input en minúsculas
+    this.filtro = this.filtrarUsuariosReg.toLowerCase();
+    if (this.filtro.length != 0) {
+      this.authService.listaFiltro().subscribe(data => {
+        const usuariosFiltrados = data.filter(usuario => {
+          const apellidoUsuario = usuario.apellido.toLowerCase();
+          // Búsqueda aproximada
+          return apellidoUsuario.includes(this.filtro);
+        });
+        this.usuariosFiltrados = usuariosFiltrados;
       });
-  
-      // Asigna los usuarios filtrados a la variable
-      this.usuariosRegistrados = { ...data, content: usuariosFiltrados };
-    });
+    } else {
+      this.listaUsuarios();
+    }
+
   }
 
   listaUsuariosPaginados(p: number): void {
@@ -57,24 +71,25 @@ export class UsuariosRegistradosComponent implements OnInit {
       this.usuariosRegistrados = data;
     });
   }
-  
-  abrirModalEliminarUsuario(){
+
+  abrirModalEliminarUsuario() {
     this.modalEliminarUsuario = "block";
   }
-  cerrarModalEliminarUsuario(){
+  cerrarModalEliminarUsuario() {
     this.modalEliminarUsuario = "none";
   }
 
-  delete(id?: number){
-    if(id != undefined){
-        this.authService.delete(id).subscribe(
-        data =>{alert("✅ Usuario borrado correctamente");
+  delete(id?: number) {
+    if (id != undefined) {
+      this.authService.delete(id).subscribe(
+        data => {
+          alert("✅ Usuario borrado correctamente");
           this.listaUsuariosPaginados(this.backendPage);
-        }, err =>{
+        }, err => {
           alert("No se pudo borrar el usuario");
         }
       )
-    } else{
+    } else {
       alert("No se pudo borrar el usuario");
     }
   }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -10,22 +10,22 @@ import { Usuarios } from '../model/usuarios';
   providedIn: 'root'
 })
 export class AuthService {
-  
+
 
   authURL = 'http://localhost:8080/auth/'
   constructor(private httpClient: HttpClient) { }
 
-  public nuevo(nuevoUsuario: NuevoUsuario): Observable<any>{
+  public nuevo(nuevoUsuario: NuevoUsuario): Observable<any> {
     return this.httpClient.post<any>(this.authURL + 'nuevo', nuevoUsuario);
   }
 
-  public login(loginUsuario: LoginUsuario): Observable<JwtDto>{
+  public login(loginUsuario: LoginUsuario): Observable<JwtDto> {
     return this.httpClient.post<JwtDto>(this.authURL + 'login', loginUsuario);
   }
 
-  public getUsuario(id: number): Observable<NuevoUsuario>{
+  public getUsuario(id: number): Observable<NuevoUsuario> {
     return this.httpClient.get<NuevoUsuario>(this.authURL + `detail/${id}`);
-    
+
   }
 
   //con esta declaracion traigo los datos del usuario de mi back
@@ -38,39 +38,43 @@ export class AuthService {
   }
 
   //agregado para poder completar el CRUD
-  public details(id: number): Observable<NuevoUsuario>{
+  public details(id: number): Observable<NuevoUsuario> {
     return this.httpClient.get<NuevoUsuario>(this.authURL + `detail/${id}`);
   }
 
-  
-  public update(id: number, nuevoUsuario: NuevoUsuario): Observable<any>{
+
+  public update(id: number, nuevoUsuario: NuevoUsuario): Observable<any> {
     return this.httpClient.put<any>(this.authURL + `update/${id}`, nuevoUsuario);
   }
 
-  public updatepass(id: number, nuevoUsuario: NuevoUsuario): Observable<any>{
+  public updatepass(id: number, nuevoUsuario: NuevoUsuario): Observable<any> {
     return this.httpClient.put<any>(this.authURL + `updatepass/${id}`, nuevoUsuario);
   }
 
-  public updateimg(id: number, nuevoUsuario: NuevoUsuario): Observable<any>{
+  public updateimg(id: number, nuevoUsuario: NuevoUsuario): Observable<any> {
     return this.httpClient.put<any>(this.authURL + `updateimg/${id}`, nuevoUsuario);
   }
 
-  public delete(id: number): Observable<any>{
+  public delete(id: number): Observable<any> {
     return this.httpClient.delete<any>(this.authURL + `delete/${id}`);
   }
-  
-  public uploadImage(imageData: string, id:number): Observable<any> {
-    const headers = new HttpHeaders({'Content-Type' : 'application/json'});
-    const options = { headers: headers};
-    const body = { imageData: imageData};
+
+  public uploadImage(imageData: string, id: number): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const options = { headers: headers };
+    const body = { imageData: imageData };
     return this.httpClient.post<any>(this.authURL + `update/${id}`, body, options);
   }
 
   public lista(page: number, size: number): Observable<NuevoUsuario[]> {
     const params = new HttpParams()
-        .set('page', page.toString())
-        .set('size', size.toString());
+      .set('page', page.toString())
+      .set('size', size.toString());
 
     return this.httpClient.get<NuevoUsuario[]>(`${this.authURL}lista`, { params });
-}
+  }
+
+  public listaFiltro(): Observable<NuevoUsuario[]> {
+    return this.httpClient.get<NuevoUsuario[]>(this.authURL + 'listaFiltro');
+  }
 }

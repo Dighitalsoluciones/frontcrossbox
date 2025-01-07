378f7aebbf4ee15b5047f4cd342c9044860c7de2
Dighitalsoluciones <hernanglomba@gmail.com>
2025-01-07 14:27:23 -0300

    Feature: Se agrega banner con link a la nueva tienda online, deshabilitado por el monento el link

---
+++ src/app/componentes/notification-banner/notification-banner.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-notification-banner',
+  templateUrl: './notification-banner.component.html',
+  styleUrls: ['./notification-banner.component.css']
+})
+export class NotificationBannerComponent {
+
+  @Input() message: string = '';
+  @Input() targetUrl: string = '';
+  @Input() imageUrl: string = '';
+  isVisible = true;
+
+  constructor(private router: Router) { }
+
+  handleClick() {
+    //Descomentar luego una vez que nos den el ok para accedan a la tienda
+    // if (this.targetUrl.startsWith('http')) {
+    //   window.location.href = this.targetUrl;
+    // } else {
+    //   this.router.navigate([this.targetUrl]);
+    // }
+  }
+
+  close(event: Event) {
+    event.stopPropagation();
+    this.isVisible = false;
+  }
+}

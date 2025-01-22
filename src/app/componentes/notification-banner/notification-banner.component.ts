c76e1a420f7c5fed6914775d722b6e964fe57a48
Dighitalsoluciones <hernanglomba@gmail.com>
2025-01-22 14:48:50 -0300

    Style: Agregado de efecto llamativo a banner de tienda

--- src/app/componentes/notification-banner/notification-banner.component.ts
+++ src/app/componentes/notification-banner/notification-banner.component.ts
@@ -1,19 +1,38 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-notification-banner',
   templateUrl: './notification-banner.component.html',
-  styleUrls: ['./notification-banner.component.css']
+  styleUrls: ['./notification-banner.component.css'],
+  animations: [
+    trigger('bannerState', [
+      state('void', style({
+        transform: 'translateY(-100%)',
+        opacity: 0
+      })),
+      state('visible', style({
+        transform: 'translateY(0)',
+        opacity: 1
+      })),
+      transition('void => visible', animate('300ms ease-out')),
+      transition('visible => void', animate('200ms ease-in'))
+    ])
+  ]
 })
-export class NotificationBannerComponent {
-
+export class NotificationBannerComponent implements OnInit {
   @Input() message: string = '';
   @Input() targetUrl: string = '';
   @Input() imageUrl: string = '';
   isVisible = true;
+  animationState = 'visible';
 
   constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.animationState = 'visible';
+  }
 
   handleClick() {
     if (this.targetUrl.startsWith('http')) {
@@ -25,6 +44,9 @@ export class NotificationBannerComponent {
 
   close(event: Event) {
     event.stopPropagation();
-    this.isVisible = false;
+    this.animationState = 'void';
+    setTimeout(() => {
+      this.isVisible = false;
+    }, 200);
   }
 }
